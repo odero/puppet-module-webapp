156cd960f2bd1121d700f07dacbb26ab7d689ba6
Maarten <maarten@byte.nl>
2013-07-12 13:33:43 +0200

    Add environment variables for gunicorn

--- manifests/python/instance.pp
+++ manifests/python/instance.pp
@@ -15,7 +15,8 @@ define webapp::python::instance($domain,
                                 $monit_cpu_limit=50,
                                 $ssl=false,
                                 $ssl_certificate="",
-                                $ssl_certificate_key="") {
+                                $ssl_certificate_key="",
+                                $environment={}) {
 
   $venv = "${webapp::python::venv_root}/$name"
   $src = "${webapp::python::src_root}/$name"
@@ -68,6 +69,7 @@ define webapp::python::instance($domain,
     paste_settings => $paste_settings,
     workers => $workers,
     timeout_seconds => $timeout_seconds,
+    environment => $environment,
     require => $ensure ? {
       'present' => Python::Venv::Isolate[$venv],
       default => undef,
