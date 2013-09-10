b295778921366227cd546a52e812e41259b36a4d
Maarten van Schaik <tech@byte.nl>
2013-09-10 18:07:35 +0200

    Make nginx optional

--- manifests/python.pp
+++ manifests/python.pp
@@ -3,14 +3,17 @@ class webapp::python($ensure=present,
                      $group="www-data",
                      $src_root="/usr/local/src",
                      $venv_root="/usr/local/venv",
+                     $nginx=true,
                      $nginx_workers=1,
                      $monit=true,
                      $monit_admin="",
                      $monit_interval=60) {
 
-  class { "nginx":
-    ensure => $ensure,
-    workers => $nginx_workers
+  if $nginx {
+    class { "nginx":
+      ensure => $ensure,
+      workers => $nginx_workers
+    }
   }
 
   class { "python::dev":
